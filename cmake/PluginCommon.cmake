1ef49b9582e86e4a7f5bd726ea9a886c61c073e6
zkamiyama <68386470+zkamiyama@users.noreply.github.com>
2026-09-16 22:04:52 +0900

    Test both directly integrated host modules with explicit UI modes and sanitizer coverage

--- cmake/PluginCommon.cmake
+++ cmake/PluginCommon.cmake
@@ -3,3 +3,9 @@ if(NOT TARGET boiled_egg_plugin_common)
   add_subdirectory("${CMAKE_CURRENT_LIST_DIR}/../adapters/common"
                    "${CMAKE_BINARY_DIR}/adapters/common")
 endif()
+# This include can run from both sibling adapter scopes.
+get_target_property(plugin_definitions boiled_egg_plugin_common INTERFACE_COMPILE_DEFINITIONS)
+set(BOILED_EGG_PLUGIN_HAS_UI OFF)
+if("BOILED_EGG_PLUGIN_X11=1" IN_LIST plugin_definitions)
+  set(BOILED_EGG_PLUGIN_HAS_UI ON)
+endif()
